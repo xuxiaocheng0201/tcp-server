acb9f48b63039b95e22eb6b10e42e658f0e4aa9e
xuxiaocheng <2762267080@qq.com>
2023-12-28 15:48:54 +0800

    feat(Handler): 使用宏简化创建

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub mod configuration;
 
+// extern for macro.
+pub extern crate anyhow;
 pub extern crate async_trait;
+pub extern crate tokio;
+pub extern crate tokio_util;
 pub extern crate tcp_handler;
 #[cfg(feature = "serde")]
 pub extern crate serde;
@@ -29,6 +33,19 @@ use crate::configuration::{get_addr, get_connect_sec, get_idle_sec};
 #[async_trait]
 pub trait FuncHandler<R, W>: Send where R: AsyncReadExt + Unpin + Send, W: AsyncWriteExt + Unpin + Send {
     async fn handle(&self, receiver: &mut R, sender: &mut W, cipher: AesCipher) -> Result<AesCipher>;
+}
+
+#[macro_export]
+macro_rules! func_handler {
+    ($vi: vis $name: ident, |$receiver: ident, $sender: ident, $cipher: ident| $block: expr) => {
+        $vi struct $name;
+        #[$crate::async_trait::async_trait]
+        impl<R: $crate::tokio::io::AsyncReadExt + Unpin + Send, W: $crate::tokio::io::AsyncWriteExt + Unpin + Send> $crate::FuncHandler<R, W> for $name {
+            async fn handle(&self, $receiver: &mut R, $sender: &mut W, $cipher: $crate::tcp_handler::common::AesCipher) -> $crate::anyhow::Result<AesCipher> {
+                $block
+            }
+        }
+    };
 }
 
 #[async_trait]
