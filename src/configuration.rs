e9a511725379e8c7e15774f71ed5aa7b0ffa7f4d
xuxiaocheng <2762267080@qq.com>
2024-01-01 20:08:37 +0800

    feat(Config): 延迟初始化配置，简化代码

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,46 +1,27 @@
 use std::sync::RwLock;
-use Either::Left;
-use tokio_util::either::Either;
-use tokio_util::either::Either::Right;
+use lazy_static::lazy_static;
 
 #[derive(Debug, Clone)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct Configuration {
-    #[cfg_attr(feature = "serde", serde(serialize_with = "ser_addr", deserialize_with = "de_addr"))]
-    pub addr: Either<String, &'static str>,
+    pub addr: String,
     pub connect_sec: u64,
     pub idle_sec: u64,
-}
-
-#[cfg(feature = "serde")]
-fn ser_addr<S>(addr: &Either<String, &'static str>, serializer: S) -> Result<S::Ok, S::Error> where S: serde::Serializer {
-    match addr {
-        Left(a) => serializer.serialize_str(a),
-        Right(a) => serializer.serialize_str(a),
-    }
-}
-
-#[cfg(feature = "serde")]
-fn de_addr<'de, D>(deserializer: D) -> Result<Either<String, &'static str>, D::Error> where D: serde::Deserializer<'de> {
-    use serde::Deserialize;
-    Ok(Left(String::deserialize(deserializer)?))
 }
 
 impl Default for Configuration {
     fn default() -> Self {
         Self {
-            addr: Right("localhost:0"),
+            addr: "localhost:0".to_string(),
             connect_sec: 30,
             idle_sec: 60,
         }
     }
 }
 
-static CONFIG: RwLock<Configuration> = RwLock::new(Configuration {
-    addr: Right("localhost:0"),
-    connect_sec: 30,
-    idle_sec: 60,
-});
+lazy_static! {
+    static ref CONFIG: RwLock<Configuration> = RwLock::new(Configuration::default());
+}
 
 #[inline]
 pub fn set_config(config: Configuration) {
@@ -57,10 +38,7 @@ pub fn get_config() -> Configuration {
 #[inline]
 pub fn get_addr() -> String {
     let c = CONFIG.read().unwrap();
-    match &(*c).addr {
-        Left(a) => a.clone(),
-        Right(a) => a.to_string(),
-    }
+    (*c).addr.clone()
 }
 
 #[inline]
